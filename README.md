3b4b5f2c998f7463a4af9c96ffe50e77af153977
HamdiHarahap <hamdiharahap2005@gmail.com>
2024-05-12 21:20:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Portofolio
+
+this is my portfolio website, created using the bootstrap framework
+
+![portofolio](https://github.com/HamdiHarahap/my-portfolio/assets/162338226/6f4461da-3a3b-45fd-8917-d3497057c2d3)
---
+++ js/main.js
@@ -0,0 +1,24 @@
+feather.replace()
+
+const themes = document.querySelectorAll('.theme')
+const moon = document.querySelector('.moon')
+const sun = document.querySelector('.sun')
+const ui = document.querySelector('.ui')
+const web = document.querySelector('.web')
+const lines = document.querySelectorAll('.line')
+const body = document.body
+
+themes.forEach((theme) => {
+    theme.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+        moon.classList.toggle('hidden');
+        sun.classList.toggle('flex');
+        ui.classList.toggle('bg-dark')
+        web.classList.toggle('bg-dark')
+        Swal.fire("you change the theme!");
+
+        lines.forEach((line) => {
+            line.classList.toggle('line-light')
+        })
+    });
+});
